bc8b5e4720f49037b757a6f14132ddea18e27f16
Evgeniy Karasev <karasevevg@gmail.com>
2021-07-31 12:01:16 +0300

    implemented Wiremock Stub for getUserTest

--- src/test/java/com/cronnoss/apiframeworksretrofitrestassured/restassured/RestAssuredTests.java
+++ src/test/java/com/cronnoss/apiframeworksretrofitrestassured/restassured/RestAssuredTests.java
@@ -1,9 +1,14 @@
 package com.cronnoss.apiframeworksretrofitrestassured.restassured;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.json.simple.JSONObject;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,13 +16,45 @@ import static io.restassured.RestAssured.given;
 
 @SpringBootTest
 public class RestAssuredTests {
+
+    private static WireMockServer wireMockServer = new WireMockServer(WireMockConfiguration.options()
+            .port(5050));
+
+    @BeforeAll
+    public static void setUpMockServer() {
+        wireMockServer.start();
+
+        WireMock.configureFor("localhost", 5050);
+        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/api/users/2"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withBody("{\n" +
+                                "    \"data\": {\n" +
+                                "        \"id\": 2,\n" +
+                                "        \"email\": \"janet.weaver@reqres.in\",\n" +
+                                "        \"first_name\": \"Janet\",\n" +
+                                "        \"last_name\": \"Weaver\",\n" +
+                                "        \"avatar\": \"https://reqres.in/img/faces/2-image.jpg\"\n" +
+                                "    },\n" +
+                                "    \"support\": {\n" +
+                                "        \"url\": \"https://reqres.in/#support-heading\",\n" +
+                                "        \"text\": \"To keep ReqRes free, contributions towards server costs are appreciated!\"\n" +
+                                "    }\n" +
+                                "}")));
+    }
+
+    @AfterAll
+    public static void tearDownMockServer() {
+        wireMockServer.stop();
+    }
+
     @Test
     void getUserTest() {
         Response response = given()
                 .contentType(ContentType.JSON)
                 .when()
-                .get("https://reqres.in/api/users/2")
-//				.get("http://localhost:5050/api/users/2")
+//                .get("https://reqres.in/api/users/2")
+                .get("http://localhost:5050/api/users/2")
                 .then()
                 .extract().response();
 
